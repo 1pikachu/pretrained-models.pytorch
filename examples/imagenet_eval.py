b79cd840a26af15494cd8e69b5b0202af98f8d7d
jundu <jun.du@intel.com>
2023-06-01 02:06:57 -0700

    update pretrained models

--- examples/imagenet_eval.py
+++ examples/imagenet_eval.py
@@ -19,6 +19,12 @@ sys.path.append('.')
 import pretrainedmodels
 import pretrainedmodels.utils
 
+try:
+    from context_func import context_func
+except ModuleNotFoundError as e:
+    print("!!!pls check how to add context_func.py from launch_benchmark.sh")
+    sys.exit(0)
+
 model_names = sorted(name for name in pretrainedmodels.__dict__
                      if not name.startswith("__")
                      and name.islower()
@@ -57,6 +63,28 @@ parser.add_argument('--do-not-preserve-aspect-ratio',
                     dest='preserve_aspect_ratio',
                     help='do not preserve the aspect ratio when resizing an image',
                     action='store_false')
+# OOB
+parser.add_argument('--device', default="cpu", choices=["cpu", "xpu", "cuda"])
+parser.add_argument('--jit', action='store_true', default=False,
+                    help='enable Intel_PyTorch_Extension JIT path')
+parser.add_argument('--nv_fuser', action='store_true', default=False)
+parser.add_argument('-i', '--num_iter', default=0, type=int, metavar='N',
+                    help='number of total iterations to run')
+parser.add_argument('-w', '--num_warmup', default=0, type=int, metavar='N',
+                    help='number of warmup iterations to run')
+parser.add_argument('--precision', type=str, default="float32",
+                    help='precision, float32, int8, float16')
+parser.add_argument("-t", "--profile", action='store_true',
+                    help="Trigger profile on current topology.")
+parser.add_argument("--performance", action='store_true',
+                    help="measure performance only, no accuracy.")
+parser.add_argument("--dummy", action='store_true',
+                    help="using  dummu data to test the performance of inference")
+parser.add_argument('--channels_last', type=int, default=1,
+                    help='use channels last format')
+parser.add_argument('--config_file', type=str, default="./conf.yaml",
+                    help='config file for int8 tuning')
+
 parser.set_defaults(preserve_aspect_ratio=True)
 best_prec1 = 0
 
@@ -64,6 +92,12 @@ best_prec1 = 0
 def main():
     global args, best_prec1
     args = parser.parse_args()
+    print(args)
+
+    if args.device == "xpu":
+        import intel_extension_for_pytorch
+    elif args.device == "cuda":
+        torch.backends.cuda.matmul.allow_tf32 = False
 
     # create model
     print("=> creating model '{}'".format(args.arch))
@@ -72,7 +106,7 @@ def main():
         model = pretrainedmodels.__dict__[args.arch](num_classes=1000,
                                                      pretrained=args.pretrained)
     else:
-        model = pretrainedmodels.__dict__[args.arch]()
+        model = pretrainedmodels.__dict__[args.arch](pretrained=None)
 
     # optionally resume from a checkpoint
     if args.resume:
@@ -87,7 +121,8 @@ def main():
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
-    cudnn.benchmark = True
+    if args.device == "cuda":
+        cudnn.benchmark = True
 
     # Data loading code
     # traindir = os.path.join(args.data, 'train')
@@ -110,33 +145,81 @@ def main():
     # else:
     #     scale = 0.875
     scale = 0.875
+    opt = pretrainedmodels.pretrained_settings[args.arch]["imagenet"]
 
     print('Images transformed from size {} to {}'.format(
-        int(round(max(model.input_size) / scale)),
-        model.input_size))
+        int(round(max(opt["input_size"]) / scale)),
+        opt["input_size"]))
+    #print('Images transformed from size {} to {}'.format(
+    #    int(round(max(model.input_size) / scale)),
+    #    model.input_size))
 
     val_tf = pretrainedmodels.utils.TransformImage(
-        model,
+        opt,
         scale=scale,
         preserve_aspect_ratio=args.preserve_aspect_ratio
     )
 
-    val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(valdir, val_tf),
-        batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
+    if not args.dummy:
+        val_loader = torch.utils.data.DataLoader(
+            datasets.ImageFolder(valdir, val_tf),
+            batch_size=args.batch_size, shuffle=False,
+            num_workers=args.workers, pin_memory=True)
+    else:
+        val_loader = ""
 
     # define loss function (criterion) and optimizer
-    criterion = nn.CrossEntropyLoss().cuda()
+    criterion = nn.CrossEntropyLoss().to(args.device)
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
 
-    model = torch.nn.DataParallel(model).cuda()
+    #model = torch.nn.DataParallel(model).cuda()
+    model = model.to(args.device)
+
+    if args.device == "xpu":
+        datatype = torch.float16 if args.precision == "float16" else torch.bfloat16 if args.precision == "bfloat16" else torch.float
+        model = torch.xpu.optimize(model=model, dtype=datatype)
+        print("---- enable xpu optmize")
+
+    if args.channels_last or args.device == "cuda":
+        try:
+            model = model.to(memory_format=torch.channels_last)
+            print("---- use NHWC format")
+        except RuntimeError as e:
+            print("---- use normal format")
+            print("failed to enable NHWC: ", e)
+    if args.nv_fuser:
+       args.fuser_mode = "fuser2"
+    else:
+       args.fuser_mode = "none"
+    print("---- fuser mode:", args.fuser_mode)
+
+    image_size = pretrainedmodels.pretrained_settings[args.arch]["imagenet"]["input_size"]
+    images = torch.randn(args.batch_size, *image_size).cuda()
+    if args.jit:
+        try:
+            model = torch.jit.trace(model, images, check_trace=False)
+            print("---- JIT trace enable.")
+        except (RuntimeError, TypeError) as e:
+            print("---- JIT trace disable.")
+            print("failed to use PyTorch jit mode due to: ", e)
 
     if args.evaluate:
-        validate(val_loader, model, criterion)
+        if args.precision == "float16":
+            amp_dtype = torch.float16
+            amp_enabled = True
+        elif args.precision == "bfloat16":
+            amp_dtype = torch.bfloat16
+            amp_enabled = True
+        else:
+            amp_dtype = torch.float32
+            amp_enabled = False
+        print("---- amp_enable:{}, amp_dtype:{}".format(amp_enabled, amp_dtype))
+
+        with torch.autocast(device_type=args.device, enabled=amp_enabled, dtype=amp_dtype):
+            validate(val_loader, model, criterion)
         return
 
     for epoch in range(args.start_epoch, args.epochs):
@@ -174,7 +257,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
         # measure data loading time
         data_time.update(time.time() - end)
 
-        target = target.cuda()
+        target = target.to(args.device)
         input_var = torch.autograd.Variable(input)
         target_var = torch.autograd.Variable(target)
 
@@ -218,38 +301,66 @@ def validate(val_loader, model, criterion):
         # switch to evaluate mode
         model.eval()
 
-        end = time.time()
-        for i, (input, target) in enumerate(val_loader):
-            target = target.cuda()
-            input = input.cuda()
+        if args.dummy:
+            profile_len = args.num_iter // 2
+            loopper = zip(range(args.num_iter), range(args.num_iter))
+        else:
+            profile_len = min(len(val_loader), args.num_iter) // 2
+            loopper = val_loader
+        for i, (input, target) in enumerate(loopper):
+            if i >= args.num_iter:
+                break
+            if args.dummy:
+                image_size = pretrainedmodels.pretrained_settings[args.arch]["imagenet"]["input_size"]
+                input = torch.randn(args.batch_size, *image_size)
+            input = input.to(args.device)
 
+            if args.channels_last or args.device == "cuda":
+                input = input.to(args.device)
+
+            start = time.time()
             # compute output
-            output = model(input)
-            loss = criterion(output, target)
-
-            # measure accuracy and record loss
-            prec1, prec5 = accuracy(output.data, target.data, topk=(1, 5))
-            losses.update(loss.data.item(), input.size(0))
-            top1.update(prec1.item(), input.size(0))
-            top5.update(prec5.item(), input.size(0))
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
+            with context_func(args.profile if i == profile_len else False, args.device, args.fuser_mode, i == profile_len) as prof:
+                output = model(input)
+                #loss = criterion(output, target)
+            if args.device == "xpu":
+                torch.xpu.synchronize()
+            elif args.device == "cuda":
+                torch.cuda.synchronize()
             end = time.time()
 
-            if i % args.print_freq == 0:
-                print('Test: [{0}/{1}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                       i, len(val_loader), batch_time=batch_time, loss=losses,
-                       top1=top1, top5=top5))
+            print("Iteration: {}, inference time: {} sec.".format(i, end - start), flush=True)
+            if i >= args.num_warmup:
+                batch_time.update(end - start)
 
-        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-              .format(top1=top1, top5=top5))
+        batch_size = args.batch_size
+        latency = batch_time.avg / batch_size * 1000
+        perf = batch_size/batch_time.avg
+        print('inference latency: %3.3f ms'%latency)
+        print('inference Throughput: %3.3f fps'%perf)
 
-        return top1.avg, top5.avg
+            # measure accuracy and record loss
+            #prec1, prec5 = accuracy(output.data, target.data, topk=(1, 5))
+            #losses.update(loss.data.item(), input.size(0))
+            #top1.update(prec1.item(), input.size(0))
+            #top5.update(prec5.item(), input.size(0))
+
+            # measure elapsed time
+            #batch_time.update(start - end)
+
+            #if i % args.print_freq == 0:
+            #    print('Test: [{0}/{1}]\t'
+            #          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+            #          'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+            #          'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+            #          'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+            #           i, len(val_loader), batch_time=batch_time, loss=losses,
+            #           top1=top1, top5=top5))
+
+        #print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+        #      .format(top1=top1, top5=top5))
+
+        #return top1.avg, top5.avg
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
